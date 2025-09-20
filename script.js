5ddc7cfe2acb7cacda3cfd57fbfeec1cfdd06f0a
Hadi <n01774009@humber.ca>
2025-09-20 07:23:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var studentName = "John Doe"; // String variable
+let studentAge = 20; // Number variable
+const isEnrolled = true; // Boolean variable
+
+console.log("Student Name: " + studentName);
+console.log("Student Age: " + studentAge);
+console.log("Is Enrolled: " + isEnrolled);
+
+stringVar = "Hello, World!";
+numberVar = 42;
+booleanVar = false;
+arrayVar = [1, 2, 3, 4, 5];
+objectVar = { key1: "value1", key2: "value2" };
+
+console.log("String Variable: " + stringVar);
+console.log("Number Variable: " + numberVar);
+console.log("Boolean Variable: " + booleanVar);
+console.log("Array Variable: " + arrayVar);
+console.log("Object Variable: " + JSON.stringify(objectVar));
+
+5+10; // Addition
+20-5; // Subtraction
+4*3; // Multiplication
+20/4; // Division
+10%3; // Modulus
+<5; // Less than
+>10; // Greater than
+<=15; // Less than or equal to
+>=20; // Greater than or equal to
+==10; // Equal to
+!=5; // Not equal to
+&&true; // Logical AND
+||false; // Logical OR
+!true; // Logical NOT
